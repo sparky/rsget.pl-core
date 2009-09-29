d301b2181cdd0779838471fbf7ce4d7369f24bb9
Przemysław Iskra <sparky@pld-linux.org>
2009-09-29 22:33:50 +0000

    - prepare for config file
    git-svn-id: http://svn.pld-linux.org/svn/toys/rsget.pl@10637 11da6c6d-24c7-0310-97e0-b6d17e038e62

--- rsget.pl
+++ rsget.pl
@@ -15,6 +15,7 @@ BEGIN {
 use Time::HiRes;
 use RSGet::Line;
 use RSGet::Tools;
+use RSGet::AutoUpdate;
 use RSGet::Processor;
 use RSGet::Curl;
 use RSGet::FileList;
@@ -25,44 +26,58 @@ use RSGet::Dispatch;
 use RSGet::ListManager;
 $SIG{CHLD} = "IGNORE";
 
+set_rev qq$Id$;
+
 %settings = (
+	auto_update => undef,
 	backup => "copy,move",
-	# backup_suf => "~",
+	backup_suf => undef,
 	logging => 0,
 	list_lock => '.${file}.swp', # vim-like swap file
+	http_port => undef,
+	list_file => undef,
 	errorlog => 0,
 	outdir => '.',
 	workdir => '.',
 );
 
+my @save_ARGV = @ARGV;
+
+sub set
+{
+	my ( $key, $val ) = @_;
+	if ( exists $settings{ $key } ) {
+		$settings{ $key } = $val;
+	} else {
+		warn "Option '$key' does not exist\n";
+	}
+}
+
 # read options
-my $http = undef;
-my $flist = undef;
 while ( my $arg = shift @ARGV ) {
-	if ( $arg eq '-i' ) {
+	if ( $arg eq '-h' ) {
+		die "No help here, sorrt. For real help read documentation.\n";
+	} elsif ( $arg eq '-i' ) {
 		my $ifs = shift @ARGV || die "argument missing";
 		RSGet::Dispatch::add_interface( $ifs );
 	} elsif ( $arg eq '-p' ) {
-		require RSGet::HTTPServer;
-		my $port = shift @ARGV || die "port missing";
-		$http = new RSGet::HTTPServer( $port );
-		p "HTTP server " . ( $http ? "started on port $port" : "failed" ) ;
-	} elsif ( $arg eq '-s' ) {
-		my $data = shift @ARGV;
-		my $o = eval "{ $data }";
-		die "Can't process settings: $@\n" if $@;
-		if ( $o and ref $o ) {
-			hadd \%settings, %$o;
-		}
+		set( "http_port", shift @ARGV || die "port missing" );
 	} elsif ( $arg =~ s/^--(.*?)=// ) {
-		$settings{ $1 } = $arg;
+		set( $1, $arg );
 	} elsif ( $arg =~ s/^--(.*)// ) {
 		my $key = $1;
 		my $var = shift @ARGV;
 		die "argument missing" unless defined $var;
-		$settings{ $key } = $var;
+		set( $key, $var );
 	} else {
-		$flist = $arg;
+		set( "list_file", $arg );
+	}
+}
+
+if ( $settings{auto_update} ) {
+	if ( RSGet::AutoUpdate::update() ) {
+		warn "Update successfull, restarting\n";
+		exec $0, @save_ARGV;
 	}
 }
 if ( keys %settings ) {
@@ -70,7 +85,18 @@ if ( keys %settings ) {
 	hprint \%settings;
 }
 
-RSGet::FileList::set_file( $flist );
+RSGet::FileList::set_file( $settings{list_file} );
+
+my $http = undef;
+if ( $settings{http_port} ) {
+	require RSGet::HTTPServer;
+	$http = new RSGet::HTTPServer( $settings{http_port} );
+	p "HTTP server " . ( $http ? "started on port $settings{http_port}" : "failed" ) ;
+}
+
+if ( $settings{interfaces} ) {
+	RSGet::Dispatch::add_interface( $settings{interfaces} );
+}
 
 new RSGet::Line();
 
