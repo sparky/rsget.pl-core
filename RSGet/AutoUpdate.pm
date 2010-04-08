a71a8d08b7f7e7a6e4482217c3c96c437dda18de
sparky <sparky@pld-linux.org>
2010-04-08 02:02:48 +0200

    - updater sketch

--- RSGet/AutoUpdate.pm
+++ RSGet/AutoUpdate.pm
@@ -5,60 +5,60 @@ package RSGet::AutoUpdate;
 #		This program is free software,
 # you may distribute it under GPL v2 or newer.
 
+# XXX: this file is a rough sketch, not functional
+
 use strict;
 use warnings;
 use RSGet::Tools;
-use Cwd;
-use File::Path;
-
-set_rev qq$Id$;
+use RSGet::DB;
+use RSGet::Plugin;
+use Digest::MD5 qw(md5_hex);
 
 def_settings(
-	use_svn => {
-		desc => "Set to 'update' to automatically update rsget.pl components from SVN. " .
-			"Set to 'yes' to use downloaded components without updating first.",
-		default => "no",
-		allowed => qr{no|yes|update},
-	},
-	svn_uri => {
-		desc => "SVN path to rsget.pl source code.",
-		default => 'http://svn.pld-linux.org/svn/toys/rsget.pl',
-		allowed => qr{(svn|https?)://.{4,}},
+	plugin_uri => {
+		desc => "http path to rsget.pl plugins updater.",
+		default => 'http://rsget.pl/download/plugins.php',
+		allowed => qr{https?)://.{4,}},
 	},
 );
 
 sub update
 {
-	unless ( require_prog( "svn" ) ) {
-		warn "SVN client required\n";
-		return 0;
-	}
-	my $start_dir = getcwd();
-	my $svn_dir = $main::local_path;
-	mkpath $svn_dir unless -d $svn_dir;
-	chdir $svn_dir or die "Can't chdir to '$svn_dir'\n";
+	my %plugins = RSGet::DB::get( "plugins", [qw(name md5)] );
 
-	print "Updating from SVN:\n";
-	local $ENV{LC_ALL} = "C";
-	my $svn_uri = setting("svn_uri");
-	my $updated = 0;
-	my $last = "";
-	open SVN, "-|", "svn", "co", $svn_uri, ".";
-	while ( <SVN> ) {
-		print "    " . $_;
-		chomp;
-		$updated++;
-		$last = $_;
-	}
-	close SVN;
-	unless ( $last =~ /Checked out revision \d+/ ) {
-		warn "Update failed ?\n";
-	}
-	chdir $start_dir;
+	RSGet::Curl::new(
+		setting( "plugin_uri" ),
+		\&process,
+		post => \%plugins,
+	);
 
-	$updated--;
-	return undef unless $updated >= 0;
-	return $updated;
+}
+
+sub process
+{
+	s/^\s*<!--.*?-->\s*//s;
+
+	while ( s/\s*<plugin:([0-9a-f]+|missing)\s+name="(\S+)">//s ) {
+		my $md5 = $1;
+		my $name = $2;
+		s#^(.*?)</plugin:$md5>##s;
+		my $body = $1;
+
+		if ( $md5 eq "missing" ) {
+			RSGet::DB::del( "plugins", { name => $name } );
+		} else {
+			my $m = md5_hex( $body );
+			if ( $m ne $md5 ) {
+				print "$name Error\n";
+				# do nothing
+			} else {
+				print "$name OK\n";
+				my @plugin = RSGet::Plugin::preprocess( $name, $body );
+				RSGet::DB::set( "plugins", [qw(name md5 body)], \@plugin );
+			}
+		}
+	}
+
 }
 
 1;
