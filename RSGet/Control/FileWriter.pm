acb1c50c6acb0510ba0a06764642b87d05596412
sparky <sparky@pld-linux.org>
2010-04-12 00:33:58 +0200

    - placeholder for file writing code (this one will allow downloading   files from multiple sources at once)

---
+++ RSGet/Control/FileWriter.pm
@@ -0,0 +1,20 @@
+package RSGet::FileWriter;
+# This file is an integral part of rsget.pl downloader.
+#
+# 2010 (c) Przemysław Iskra <sparky@pld-linux.org>
+#		This program is free software,
+# you may distribute it under GPL v2 or newer.
+
+use strict;
+use warnings;
+use RSGet::Common;
+use RSGet::DB;
+use File::Copy;
+use File::Path;
+use Fcntl qw(SEEK_SET);
+
+
+
+1;
+
+# vim: ts=4:sw=4
