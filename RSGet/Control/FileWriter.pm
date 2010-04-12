3d63610dbfccd9882db6681da69bf3a3d58cf858
sparky <sparky@pld-linux.org>
2010-04-12 02:13:51 +0200

    - added some code

--- RSGet/Control/FileWriter.pm
+++ RSGet/Control/FileWriter.pm
@@ -13,6 +13,65 @@ use File::Copy;
 use File::Path;
 use Fcntl qw(SEEK_SET);
 
+my %files;
+
+sub new
+{
+	my $class = shift;
+
+	my $self = {
+	};
+
+	return bless $self, $class;
+}
+
+sub start
+{
+	my $self = shift;
+
+	open my $handle, "+<:raw", $file_name;
+	my $file = {
+		handle => $handle,
+		users => 1,
+		id => $file_name,
+	};
+	$files{ $file->{id} } = $file;
+}
+
+sub seek
+{
+	my $self = shift;
+	$self->{position} = shift;
+}
+
+sub push
+{
+	my $self = shift;
+
+	my $fh = $self->{file}->{handle};
+	seek $fh, $self->{position}, SEEK_SET;
+	print $fh $_[0];
+
+	$self->{position} += length $_[0];
+}
+
+sub end
+{
+	my $self = shift;
+
+	if ( --$self->{file}->{users} ) {
+		return;
+	}
+
+	close $self->{file}->{handle};
+	delete $files{ $self->{file}->{id} };
+}
+
+sub DESTROY
+{
+	my $self = shift;
+	$self->end();
+}
 
 
 1;
