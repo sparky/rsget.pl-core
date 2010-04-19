25c8443b20c99f62c49624b61613d42e2d71d6e9
sparky <sparky@pld-linux.org>
2010-04-19 03:09:11 +0200

    - simplified - objects are stored in groups now

--- RSGet/MortalObject.pm
+++ RSGet/MortalObject.pm
@@ -7,109 +7,135 @@ package RSGet::MortalObject;
 
 use strict;
 use warnings;
-use RSGet::Tools;
-set_rev qq$Id: Wait.pm 10652 2009-10-02 15:28:26Z sparky $;
+use RSGet::Common;
 
 # This is object holder, which will destroy the object if it doesn't
 # receive heartbeat for some amount of time. It is used to prevent leaking
 # memory, expecially in http interface.
 
-my %holders;
-my $last_id = 0;
+my %group;
 
+# create new MO group
+# accepts default values
 sub new
 {
 	my $class = shift;
+	my %opts = @_;
+
+	my $gid;
+	do {
+		$gid = irand 1 << 32;
+	} while ( exists $group{ $gid } );
+
+	my $group = $group{ $gid } = {};
+
+	my $self = \%opts;
+	$self->{gid} = $gid;
+	$self->{group} = $group;
+
+	return bless $self, $class;
+}
+
+# free all objects belonging to this group
+sub DESTROY
+{
+	my $self = shift;
+
+	delete $group{ $self->{gid} };
+}
+
+# add new object to mortal group
+sub add
+{
+	my $self = shift;
 	my $obj = shift;
 	my %opts = @_;
 
+	my $group = $self->{group};
+
+	my $id;
+	do {
+		$id = irand 1 << 31;
+	} while ( exists $group->{ $id } );
+
 	my $time = time;
-
-	my $id = ++$last_id . "_" . randid();
-
+	
 	my $holder = {
 		obj => $obj,
 		start => $time,
 		last => $time,
-		die_after => $opts{die_after} || 10,
+		die_after => defined $opts{die_after} ? $opts{die_after} :
+			defined $self->{die_after} ? $self->{die_after} :
+			10,
 	};
-	$holder->{kill_after} = $time + $opts{kill_after} if $opts{kill_after};
-	$holders{ $id } = $holder;
+	my $ka = $self->{kill_after};
+	$ka = $opts{kill_after}
+		if exists $opts{kill_after};
+	$holder->{kill_at} = $time + $ka
+		if defined $ka;
 
-	my $self = \$id;
-	bless $self, $class;
+	$group->{ $id } = $holder;
 
-	return $self;
+	return $id;
 }
 
-sub from_id
-{
-	my $class = shift;
-	my $id = shift;
-
-	return undef unless exists $holders{ $id };
-	my $self = \$id;
-	bless $self, $class;
-	return $self;
-}
-
-sub obj
+# ping and return object
+# my $obj = $group->get( $id );
+# returns undef if object not found
+sub get
 {
 	my $self = shift;
-	my $id = $$self;
+	my $id = shift;
 
-	my $h = $holders{ $id } or return undef;
+	# make it perl-like number
+	$id |= 0;
+
+	my $h = $self->{group}->{ $id };
+	return undef
+		unless $h;
+
 	$h->{last} = time;
 	return $h->{obj};
 }
 
-sub id
+# remove and return object
+# my $obj = $group->del( $id );
+# returns undef if object not found
+sub del
 {
 	my $self = shift;
-	my $id = $$self;
+	my $id = shift;
 
-	return undef unless $holders{ $id };
-	return $id;
+	# make it perl-like number
+	$id |= 0;
+
+	my $h = $self->{group}->{ $id };
+	return undef
+		unless $h;
+	delete $self->{group}->{ $id };
+
+	return $h->{obj};
 }
 
-sub time_to_kill
-{
-	my $self = shift;
-	my $id = $$self;
-
-	my $h = $holders{ $id } or return undef;
-	return undef unless $h->{kill_after};
-	return $h->{kill_after} - time;
-}
-
-sub heartbeat
-{
-	my $self = shift;
-	my $id = $$self;
-
-	my $h = $holders{ $id } or return undef;
-	$h->{last} = time;
-
-	return 1;
-}
-
+# update objects
+# should be called once every second
 sub update
 {
 	my $time = time;
 
-	foreach my $id ( keys %holders ) {
-		my $h = $holders{ $id };
-		if ( $h->{last} + $h->{die_after} < $time ) {
-			p "Mortal $id died\n" if verbose( 4 );
-			delete $h->{obj};
-			delete $holders{ $id };
-		} elsif ( $h->{kill_after} and $h->{kill_after} < $time ) {
-			p "Mortal $id killed\n" if verbose( 4 );
-			delete $h->{obj};
-			delete $holders{ $id };
+	foreach my $group ( values %group ) {
+		foreach my $id ( keys %$group ) {
+			my $h = $group->{ $id };
+			if ( $h->{last} + $h->{die_after} < $time ) {
+				p "Mortal $id died\n" if verbose( 4 );
+				delete $group->{ $id };
+			} elsif ( $h->{kill_after} and $h->{kill_after} < $time ) {
+				p "Mortal $id killed\n" if verbose( 4 );
+				delete $group->{ $id };
+			}
 		}
 	}
-	RSGet::Line::status( 'mortals' => scalar keys %holders );
+	#RSGet::Line::status( 'mortals' => scalar keys %holders );
 }
 
 1;
