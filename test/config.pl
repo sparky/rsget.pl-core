ff2dc4d104a549402133f3ccbd8508d63722ae75
sparky <sparky@pld-linux.org>
2010-04-27 23:23:43 +0200

    - more tests

--- test/config.pl
+++ test/config.pl
@@ -22,17 +22,29 @@ RSGet::Config::register_settings(
 	test_cmd => {
 		default => 'test: [$(ls -l)]',
 	},
+	test_l => {
+		default => "3, 4",
+	},
+	test_list => {
+		default => '1, 2, %{test_l}, %{num}, %(5+1), $(expr 5 + 2)',
+	},
 );
 RSGet::Config::init();
 
 RSGet::Config::set( undef, "foo", 'test: %{p}{test}' );
 
-my @macros = qw(test_macro test_env test_perl test_cmd foo);
+my @macros = qw(test_macro test_env test_perl test_cmd foo test_l);
 foreach ( @macros ) {
 	my $v = RSGet::Config::get( undef, $_ );
 	print "$_: $v\n";
 }
 
+my $e = RSGet::Config::expand( undef, "[ %{p} %{test} %{foo} %{something} ]" );
+print "expand: $e\n";
 
+my @l = RSGet::Config::get_list( undef, "test_list" );
+print "list:\n- ";
+print join "\n- ", @l;
+print "\n.\n";
 
 # vim:ts=4:sw=4
