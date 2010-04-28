e07bccfc488714b676dbadf26504fa9ac0cf827a
sparky <sparky@pld-linux.org>
2010-04-28 23:23:03 +0200

    - test plugin autoupdate

--- test/curl.pl
+++ test/curl.pl
@@ -2,8 +2,13 @@
 use strict;
 use warnings;
 use RSGet::Curl;
+use RSGet::Config;
+use RSGet::Plugin::AutoUpdate;
 
 use Data::Dumper;
+
+RSGet::Config::init();
+RSGet::SQL::init();
 
 my $uri = "http://rsget.pl/download/plugins";
 
@@ -15,8 +20,10 @@ sub d
 my $curl = new RSGet::Curl $uri, \&d,
 	post => {
 		"Link/YouTube" => "4fad0e233323b136787247d54a90d5d9",
-	};
+	}
+	if 0;
 
+RSGet::Plugin::AutoUpdate::update();
 while ( RSGet::Curl::perform() ) {
 	warn "Downloading...";
 	select undef, undef, undef, 0.1;
