e1638473ac50dfb5a05cf5fdedb0c1a800a3f565
sparky <sparky@pld-linux.org>
2010-04-28 23:22:34 +0200

    - mostly finished

--- RSGet/Plugin/AutoUpdate.pm
+++ RSGet/Plugin/AutoUpdate.pm
@@ -5,36 +5,52 @@ package RSGet::Plugin::AutoUpdate;
 #		This program is free software,
 # you may distribute it under GPL v2 or newer.
 
-# XXX: this file is a rough sketch, not functional
-
 use strict;
 use warnings;
+use RSGet::Config;
 use RSGet::Curl;
-use RSGet::DB;
+use RSGet::SQL;
+#use RSGet::Plugin::Plugin;
 use Digest::MD5 qw(md5_hex);
 
-def_settings(
+RSGet::Config::register_settings(
 	plugin_uri => {
 		desc => "http path to rsget.pl plugins updater.",
 		default => 'http://rsget.pl/download/plugins.php',
-		allowed => qr{https?://.{4,}},
+		allowed => qr{https?://.{4,}/.+},
 	},
 );
 
 sub update
 {
-	my %plugins = RSGet::DB::get( "plugins", [qw(name md5)] );
-
-	RSGet::Curl::new(
-		setting( "plugin_uri" ),
-		\&process,
-		post => \%plugins,
+	my $prefix = RSGet::Config::get( undef, "sql_prefix" );
+	my $arr = RSGet::SQL::dbh->selectall_arrayref(
+		"SELECT name, md5 FROM ${prefix}plugin"
 	);
+	my %plugins = map { $_->[0], $_->[1] } @$arr;
 
+	new RSGet::Curl
+		RSGet::Config::get( undef, "plugin_uri" ),
+		\&_process,
+		post => \%plugins;
+	
+	warn "Updating plugins.\n";
+	
+	return 1;
 }
 
-sub process
+sub _process
 {
+	my $error = shift;
+	my $obj = shift;
+
+	my $time = time;
+
+	local $_;
+	$_ = $obj->{body};
+
+	my $updated = 0;
+
 	s/^\s*<!--.*?-->\s*//s;
 
 	while ( s/\s*<plugin:([0-9a-f]+|missing)\s+name="(\S+)">//s ) {
@@ -44,20 +60,22 @@ sub process
 		my $body = $1;
 
 		if ( $md5 eq "missing" ) {
-			RSGet::DB::del( "plugins", { name => $name } );
+			RSGet::SQL::del( "plugin", { name => $name } );
 		} else {
 			my $m = md5_hex( $body );
 			if ( $m ne $md5 ) {
-				print "$name Error\n";
-				# do nothing
+				warn "Error whie updating plugin $name: md5 does not match.\n";
 			} else {
-				print "$name OK\n";
-				my @plugin = RSGet::Plugin::preprocess( $name, $body );
-				RSGet::DB::set( "plugins", [qw(name md5 body)], \@plugin );
+				#my @plugin = RSGet::Plugin::preprocess( $name, $body );
+				RSGet::SQL::set( "plugin", { name => $name },
+					{ md5 => $md5, body => $body, time => $time } );
+				$updated++;
 			}
 		}
 	}
 
+	warn "Updated $updated plugins.\n";
+	return;
 }
 
 1;
