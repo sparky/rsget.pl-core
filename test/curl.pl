ae4ad27b26166dcb45e964f588caea8b1f5cba87
sparky <sparky@pld-linux.org>
2010-05-04 16:58:35 +0200

    - updated for RSGet::Curl changes

--- test/curl.pl
+++ test/curl.pl
@@ -3,7 +3,9 @@ use strict;
 use warnings;
 use RSGet::Curl;
 use RSGet::Config;
+use RSGet::SQL;
 use RSGet::Plugin::AutoUpdate;
+use RSGet::Mux;
 
 use Data::Dumper;
 
@@ -21,13 +23,10 @@ my $curl = new RSGet::Curl $uri, \&d,
 	post => {
 		"Link/YouTube" => "4fad0e233323b136787247d54a90d5d9",
 	}
-	if 0;
+	if 1;
 
 RSGet::Plugin::AutoUpdate::update();
-while ( RSGet::Curl::perform() ) {
-	warn "Downloading...";
-	select undef, undef, undef, 0.1;
-}
+RSGet::Mux::main_loop();
 
 warn "Downloaded\n";
 
