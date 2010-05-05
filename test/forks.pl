6bc2e9d8e9223e51415cefb3b95ec4b0f7626651
sparky <sparky@pld-linux.org>
2010-05-06 01:20:28 +0200

    - more tests

--- test/forks.pl
+++ test/forks.pl
@@ -4,6 +4,7 @@ use warnings;
 use RSGet::Mux;
 use RSGet::Forks;
 
+sub start0
 {
 	my $pid = open my $in, "-|", "ls", "-l";
 	RSGet::Forks::add( $pid,
@@ -13,14 +14,52 @@ use RSGet::Forks;
 	);
 }
 
+sub start1
 {
-	my $pid = open my $in, 'for F in *; do echo "$F"; sleep 1; done |';
+	my $pid = open my $in, 'for F in *; do echo "$F"; usleep 100000; done |';
 	RSGet::Forks::add( $pid,
 		from_child => $in,
 		readline => \&on_line,
 		at_exit => \&bye,
 	);
 }
+
+sub start2
+{
+	my $pid = open my $in, 'for F in *; do echo -n "$F"; usleep 300000; done |';
+	RSGet::Forks::add( $pid,
+		from_child => $in,
+		readline => \&on_line,
+		at_exit => \&bye,
+	);
+}
+
+sub start3
+{
+	my $pid = open my $in, 'for F in *; do echo -n "$F"; usleep 300000; done |';
+	RSGet::Forks::add( $pid,
+		from_child => $in,
+		read => \&on_line,
+		at_exit => \&bye,
+	);
+}
+
+sub start4
+{
+	my $pid = open my $in, 'ls -l |';
+	RSGet::Forks::add( $pid,
+		from_child => $in,
+		at_exit => \&bye,
+	);
+}
+
+my @tests = (
+	\&start1,
+	\&start2,
+	\&start3,
+	\&start4,
+);
+
 
 sub on_line
 {
@@ -34,9 +73,20 @@ sub bye
 {
 	my $pid = shift;
 	my $code = shift;
+	my $text = shift;
 
-	print "Kid exited $pid: $code\n";
+	print "Kid exited $pid: $code";
+	if ( defined $text ) {
+		print " [[[\n$text]]]";
+	}
+	print "\n";
+
+	my $start = shift @tests;
+	return unless $start;
+	&$start();
 }
+
+start0();
 
 RSGet::Mux::main_loop();
 
