50162b74cdd2d3dbbd9545e739e1b9250951acff
sparky <sparky@pld-linux.org>
2010-07-27 23:42:32 +0200

    - added "code coverage" section

--- scraps/plugins.pl
+++ scraps/plugins.pl
@@ -29,7 +29,7 @@ is, they must exit for each major operation (download, wait, or wait for human
 interaction). The only way I see this is to divide each plugin in many
 functions, one function for each stage.
 
-=head1 One Thread per download ?
+=head2 One Thread per download ?
 
 If we were using one thread per download the following code could be a valid
 downloader:
@@ -55,13 +55,13 @@ start {
 
 	click "", post => { downloadLink => "show" };
 
-	download $uri, post => { download => "normal" },
+	download $uri, post => { download => "normal" };
 
 	restart $1, "free limit reached"
 		if m#You need to wait (\d+) seconds to start another download\.#;
 };
 
-=head1 One thread for all
+=head2 One thread for all
 
 In out case every get(), download(), wait() has to stop execution of this
 function. This can be easily acomplished by using eval wrapper while calling
@@ -109,7 +109,7 @@ start {
 	};
 };
 
-=head2 Continuation
+=head3 Continuation
 
 This code doesn't require any preprocessing to run. Nested functions allow
 access to variables from their parents. The only change is that each of those
@@ -117,7 +117,7 @@ methods gets a sub which they should call after performing the task.
 
 However, the deep nesting eats all our horizontal space.
 
-=head1 Same code without tabulation
+=head2 Same code without tabulation
 
 Makes it very hard to count how many sub {} we have to close.
 =cut
@@ -160,7 +160,7 @@ start {
 };
 
 
-=head1 Automatic subs
+=head2 Automatic subs
 
 We can preprocess the code, the preprocessor will automatically generate sub
 blocks for us. Each block spans between automatic sub marker and the end of the
@@ -195,7 +195,7 @@ start {
 
 };
 
-=head2 Continuation
+=head3 Continuation
 
 Looks almost like the first one !
 
@@ -212,6 +212,47 @@ captcha qr/[VALID CHARS]+/, solver => \&our_solver, sub;
 
 Without the marker preprocessor could assume execution would continue in
 &our_solver and not create the necessary sub.
+
+=cut
+
+
+
+=head1 CODE COVERAGE
+
+To keep a large number of plugins up-to-date I need some efficient and reliable
+way to track most common problems and most common uses. That's why I'm going to
+introduce "voluntary distributed code coverage analysis".
+Each rsget.pl client will have the ability to gather information about plugin
+functions being executed and upload those statistics anonymously to our
+servers. This will help us catch uncommon problems and problems that we didn't
+think of while writing the plugin. Moreover, after page update some error
+handlers are no longer used, but we can't know that without checking a very
+large number of links. With the coverage information we will be able to
+efficiently detect the dead code and remove it, keeping the plugin clean and
+always up-to-date.
+
+Each special function will generate coverage information if executed.
+There will be one function which purpose will be solely to gather coverage
+information. This will be useful for blocks that don't call special functions
+(directly).
+
+For instance, if we've got two separate matches and we would like to know
+which one does match, the code could look something like this:
+
+=cut
+
+	if (
+		expect( /foo string/ )
+			or
+		expect( /bar string/ )
+	) {
+		...
+	}
+
+=head2 Continuation
+
+Do you think this would be an invasion of someone's privacy ?
+Those statistics will be strictly voluntary and anonymous.
 
 =cut
 
