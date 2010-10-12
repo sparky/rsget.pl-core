af783e8b4851120a8ad3a39e40eba43f6d2821b8
sparky <sparky@pld-linux.org>
2010-10-12 13:03:26 +0200

    - basic config file syntax

---
+++ scraps/config.pl
@@ -0,0 +1,46 @@
+########### configuration file syntax. ###########
+
+#### changing options ####
+
+# set option statically
+option_name = $value;
+
+# set different value for each user
+option_name = by_var $user,
+	user1 => $value1,
+	user2 => $value2,
+	user3 => $value3,
+	"" => $default;
+
+# set option dynamically
+option_name = sub { <create value> };
+
+# refresh dynamic option at most once every $seconds
+option_name = sub_cache { <create value> } $seconds;
+
+# cache dynamic option separatelly for each user
+option_name = sub_cache { <create $user-specific value> } $seconds, $user;
+
+
+#### executing some code ####
+
+# cron FUNCTION, PERIOD, (DELAY);
+#
+# executes FUNCTION every PERIOD seconds
+# third argument delays execution for DELAY seconds after every PERIOD
+
+cron { [function body here] } $seconds;
+cron { [function body here] } $seconds, $delay;
+cron \&some_function, $seconds;
+
+# run every day at 3:00 UTC
+cron { ... } 24 * 60 * 60, +3 * 60 * 60;
+
+# run every minute
+cron { ... } 60;
+# run every, 15 seconds after the other one
+cron { ... } 60, +15;
+
+
+#### hooks ####
+# todo
