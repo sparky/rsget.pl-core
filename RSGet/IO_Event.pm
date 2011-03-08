549b13120185f2c20f7afb699b96476162f5ad25
sparky <sparky@pld-linux.org>
2011-03-08 21:26:51 +0100

    - added write callbacks:
    - created separate list for on-write callbacks
    - everything so far renamed to with _read suffix
    - added add_write and remove_write
    - add() and remove() operate on both lists now

--- RSGet/IO_Event.pm
+++ RSGet/IO_Event.pm
@@ -18,7 +18,7 @@ package RSGet::IO_Event;
 
 =head1 package RSGet::IO_Event
 
-Automatically call methods on new data.
+Automatically call methods on read and write events.
 
 =cut
 
@@ -27,17 +27,51 @@ use warnings;
 use IO::Select;
 use Time::HiRes ();
 
-my $select = IO::Select->new();
+my $select_read = IO::Select->new();
+my $select_write = IO::Select->new();
 
 =head2 RSGet::IO_Event->add( HANDLE, OBJECT, [METHOD] );
 
-Add OBJECT with associated HANDLE to call list. If METHOD is not specified
-io_data() will be used.
+Add OBJECT with associated HANDLE to both call lists. If METHOD is not specified
+io_read() will be used for reading and io_write() for writing.
 
 =cut
 sub add
 {
-	my $class = shift;
+	my ( $class, $handle, $object, $func ) = @_;
+	_add( $select_read, $handle, $object, $func || "io_read" );
+	_add( $select_write, $handle, $object, $func || "io_write" );
+	return 1;
+}
+
+=head2 RSGet::IO_Event->add_read( HANDLE, OBJECT, [METHOD] );
+
+Add OBJECT with associated HANDLE to read call list. If METHOD is not specified
+io_read() will be used.
+
+=cut
+sub add_read
+{
+	my ( $class, $handle, $object, $func ) = @_;
+	return _add( $select_read, $handle, $object, $func || "io_read" );
+}
+
+=head2 RSGet::IO_Event->add_write( HANDLE, OBJECT, [METHOD] );
+
+Add OBJECT with associated HANDLE to write call list. If METHOD is not specified
+io_write() will be used.
+
+=cut
+sub add_write
+{
+	my ( $class, $handle, $object, $func ) = @_;
+	return _add( $select_write, $handle, $object, $func || "io_write" );
+}
+
+# INTERNAL, actually does the job
+sub _add
+{
+	my $select = shift;
 
 	my $handle = shift;
 	my $object = shift;
@@ -55,14 +89,45 @@ sub add
 	return 1;
 }
 
+
 =head2 RSGet::IO_Event->remove( HANDLE );
 
-Remove OBJECT associated with HANDLE from call list.
+Remove OBJECT associated with HANDLE from both call lists.
 
 =cut
 sub remove
 {
-	my $class = shift;
+	my ( $class, $handle ) = @_;
+	_remove( $select_read, $handle );
+	_remove( $select_write, $handle );
+}
+
+=head2 RSGet::IO_Event->remove_read( HANDLE );
+
+Remove OBJECT associated with HANDLE from read call list.
+
+=cut
+sub remove_read
+{
+	my ( $class, $handle ) = @_;
+	_remove( $select_read, $handle );
+}
+
+=head2 RSGet::IO_Event->remove_write( HANDLE );
+
+Remove OBJECT associated with HANDLE from write call list.
+
+=cut
+sub remove_write
+{
+	my ( $class, $handle ) = @_;
+	_remove( $select_write, $handle );
+}
+
+# INTERNAL, actually does the job
+sub _remove
+{
+	my $select = shift;
 	my $handle = shift;
 
 	$handle = $handle->handle
@@ -73,12 +138,35 @@ sub remove
 
 =head2 RSGet::IO_Event::_perform();
 
-For each HANDLE ready to read call appropriate OBJECT->METHOD( $time ).
+For each HANDLE in read list ready to read call appropriate
+OBJECT->METHOD( $time ). Afterwards do the same for write list.
 
 =cut
 sub _perform
 {
-	my @io = $select->can_read( 0 );
+	return _perform_read() + _perform_write();
+}
+
+sub _perform_read
+{
+	my @io = $select_read->can_read( 0 );
+	return 0 unless @io;
+
+	my $time = Time::HiRes::time();
+	foreach my $io ( @io ) {
+		my ( $h, $obj, $func ) = @$io;
+		eval {
+			$obj->$func( $time );
+		};
+		warn $@ if $@;
+	}
+
+	return scalar @io;
+}
+
+sub _perform_write
+{
+	my @io = $select_write->can_write( 0 );
 	return 0 unless @io;
 
 	my $time = Time::HiRes::time();
