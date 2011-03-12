346f901ee4a04e83ada2fd59eacfc89db2042a3f
sparky <sparky@pld-linux.org>
2011-03-13 00:42:19 +0100

    - new: scgi server
    Mostly copied from HTTP_*.pm files, works. A lot of cleanup required.

---
+++ RSGet/SCGI_Client.pm
@@ -0,0 +1,319 @@
+package RSGet::SCGI_Client;
+# This file is an integral part of rsget.pl downloader.
+#
+# Copyright (C) 2011	Przemysław Iskra <sparky@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+use RSGet::Common qw(throw);
+use RSGet::IO;
+use RSGet::IO_Event;
+
+use constant {
+	MAX_POST_SIZE => 1 * 1024 * 1024, # 1 MB
+};
+
+my %codes = (
+	200 => "OK",
+	206 => "Partial Content",
+	401 => "Authorization Required",
+	404 => "Not Found",
+	416 => "Requested Range Not Satisfiable",
+	500 => "Internal Server Error",
+);
+
+sub create
+{
+	my $class = shift;
+	my $handle = shift;
+
+	my $io = RSGet::IO->new( $handle );
+	my $self = {
+		_io => $io,
+	};
+
+	bless $self, $class;
+
+	RSGet::IO_Event->add_read( $handle, $self );
+
+	return $self;
+}
+
+sub state
+{
+	my $self = shift;
+	my $write = shift eq "write";
+
+	my $h = $self->{_io}->handle();
+	RSGet::IO_Event->remove( $h );
+	if ( $write ) {
+		RSGet::IO_Event->add_write( $h, $self );
+	} else {
+		foreach ( keys %$self ) {
+			next if /^_/;
+			delete $self->{$_};
+		}
+	}
+}
+
+sub _lc_h
+{
+	local $_ = lc shift;
+	tr/-/_/;
+	return $_;
+}
+
+sub _ucfirst_h
+{
+	return join "-", map ucfirst, split /[-_ ]+/, lc shift;
+}
+
+sub io_read
+{
+	my $self = shift;
+	my $time = shift;
+
+	my $io = $self->{_io};
+
+	eval {
+		local $_;
+		if ( not defined $self->{head_length} ) {
+			local $/ = ":";
+			$_ = $io->readline();
+			chomp;
+			$self->{head_length} = 0+$_;
+		}
+		if ( not defined $self->{h_in} ) {
+			$_ = $io->read( $self->{head_length} + 1);
+			throw 'malformed request'
+				unless substr( $_, -1, 1, '' ) eq ',';
+			my %head = split /\0/, $_;
+			my %head_lc;
+			foreach my $key ( keys %head ) {
+				next unless $key =~ /^HTTP_(.*)$/;
+				$head_lc{ lc $1 } = $head{ $key };
+			}
+			foreach my $key ( qw(CONTENT_LENGTH) ) {
+				$head_lc{ lc $key } = $head{ $key };
+			}
+			use Data::Dumper;
+			print Dumper( \%head_lc );
+
+			$self->{h_in} = \%head_lc;
+			$self->{method} = uc $head{REQUEST_METHOD};
+			$self->{file} = $head{PATH_INFO};
+		}
+		if ( not defined $self->{post_data} ) {
+			my $len = $self->{h_in}->{content_length};
+			throw 'POST data too large'
+				if $len > MAX_POST_SIZE;
+			$self->{post_data} = $io->read( $len );
+			throw 'POST data incomplete'
+				if length $self->{post_data} != $len;
+		}
+	};
+	if ( $@ ) {
+		if ( $@ eq "RSGet::IO: no data" ) {
+			# do nothing, wait for more data
+			return;
+		} else {
+			$self->delete();
+			if ( $@ eq "RSGet::IO: read: handle closed" ) {
+				return;
+			} else {
+				die $@;
+			}
+		}
+	} else {
+		$self->process( $time );
+	}
+}
+
+sub _post2hash
+{
+	my $self = shift;
+
+	my %post;
+	if ( $self->{method} eq "POST" ) {
+		%post =
+			grep { tr/+/ /; s/%(..)/chr hex $1/eg; 1 }
+			map { split /=/, $_, 2 }
+			split /&/, $self->{post_data};
+	} elsif ( $self->{file} =~ s/\?(.*)// ) {
+		%post =
+			grep { s/%(..)/chr hex $1/eg; 1 }
+			map { split /=/, $_, 2 }
+			split /[;&]+/, $1;
+	}
+
+	return \%post;
+}
+
+sub process
+{
+	my $self = shift;
+
+	$self->{post} = _post2hash( $self );
+	$self->{code} = 200;
+	$self->{h_out} = {
+		content_type => "text/xml; charset=utf-8",
+	};
+
+	my $data;
+
+	require RSGet::HTTP_Handler;
+	my $handler = RSGet::HTTP_Handler->get( $self->{file} );
+
+	if ( $handler ) {
+		my @args = @$handler;
+		shift @args; # file match
+		my $func = shift @args;
+		eval {
+			$data = $func->( $self, @args );
+		};
+		if ( $@ ) {
+			$self->{h_out} = {
+				content_type => "text/plain; charset=utf-8",
+			};
+			if ( $@ =~ /^RSGet::HTTP_Handler: (\d{3}):\s*(.*)/ ) {
+				$self->{code} = $1;
+				( $self->{code_msg} = $2 ) =~ s/[^A-Za-z0-9 ]+//;
+				$data = "Server error: $@\n";
+			} else {
+				$self->{code} = 500;
+				$data = "Server error: $@\n";
+			}
+		}
+	} else {
+		$self->{code} = 404;
+		$data = "No handler for file '$self->{file}'\n";
+	}
+
+	$self->{h_out}->{connection} = "Keep-Alive";
+
+	$self->{code} = 500 if not exists $codes{ $self->{code} };
+	unless ( defined $data ) {
+		$self->{h_out} = {
+			content_type => "text/plain; charset=utf-8",
+		};
+		$data = $codes{ $self->{code} } . "\n"
+	}
+	if ( ref $data ) {
+		throw '$data must be a SCALAR or CODE ref, it is %s', ref $data
+			unless ref $data eq 'CODE';
+		throw 'CODE handler must set up content_length header'
+			unless exists $self->{h_out}->{content_length};
+		$self->{left} = $self->{h_out}->{content_length};
+	} else {
+		$self->{h_out}->{content_length} = length $data;
+	}
+
+	$self->{code_msg} ||= $codes{ $self->{code} };
+	my $headers = "Status: $self->{code} $self->{code_msg}\r\n";
+	foreach my $hdr ( sort keys %{ $self->{h_out} } ) {
+		$headers .= _ucfirst_h( $hdr ) . ": " . $self->{h_out}->{ $hdr } . "\r\n";
+	}
+	$headers .= "\r\n";
+
+
+	my $h = $self->{_io};
+	eval {
+		$h->write( $headers );
+	};
+	if ( ref $data ) {
+		$self->{iter} = $data;
+		$self->state( "write" );
+	} else {
+		eval {
+			$h->write( $data );
+		};
+		if ( $@ ) {
+			if ( $@ eq "RSGet::IO: busy" ) {
+				$self->{iter} = sub { throw 'DONE' };
+				$self->state( "write" );
+			} elsif ( $@ eq "RSGet::IO: write: handle closed" ) {
+				$self->delete();
+			}
+		} else {
+			$self->delete();
+		}
+	}
+
+}
+
+sub io_write
+{
+	my $self = shift;
+	my $time = shift;
+
+	my $h = $self->{_io};
+	my $i = $self->{iter};
+	eval {
+		# flush write buffer
+		$h->write();
+
+		# read more
+		local $_;
+		while ( defined ( $_ = $i->() ) ) {
+			if ( length $_ > $self->{left} ) {
+				throw 'size mismatch - tried to send more than declared';
+			}
+			$self->{left} -= length $_;
+			$h->write( $_ );
+		}
+	};
+
+	return unless $@; # busy
+	return if $@ eq 'RSGet::IO: busy';
+	return if $@ eq 'RSGet::IO: no data';
+
+	if ( $@ ge 'done' or $@ ge 'read: handle closed' ) {
+		$self->delete();
+		# no more data
+		if ( $self->{left} ) {
+			# size mismatch - close the handle
+			warn "size mismatch - not enough data sent (remaining $self->{left})\n";
+			return;
+		}
+		return;
+	}
+
+	# there was some problem
+	$self->delete();
+	if ( $@ eq "RSGet::IO: write: handle closed" ) {
+		# warn "$@\n";
+		return;
+	} else {
+		die $@;
+	}
+}
+
+
+sub delete
+{
+	my $self = shift;
+	RSGet::IO_Event->remove( $self->{_io} );
+
+	my $h = $self->{_io};
+	delete $self->{_io};
+
+	$h = $h->handle();
+	close $h;
+}
+
+1;
+
+# vim: ts=4:sw=4:fdm=marker
---
+++ RSGet/SCGI_Server.pm
@@ -0,0 +1,103 @@
+package RSGet::SCGI_Server;
+# This file is an integral part of rsget.pl downloader.
+#
+# Copyright (C) 2011	Przemysław Iskra <sparky@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+use RSGet::IO_Event;
+
+
+=head1 RSGet::SCGI_Server -- simple scgi server
+
+This package implements non-blocking scgi server.
+
+=head2 my $server = RSGet::SCGI_Server->create( WHERE );
+
+If WHERE is a number creates scgi server on tcp port WHERE, otherwise
+creates scgi server on unix socket named WHERE.
+
+=cut
+sub create
+{
+	my $class = shift;
+	my $port = shift;
+
+	my $socket;
+	if ( $port =~ m/^\d+$/ ) {
+		require IO::Socket::INET;
+		$socket = IO::Socket::INET->new(
+			Listen => 1,
+			LocalPort => $port,
+			Proto => 'tcp',
+			Listen => 32,
+			Reuse => 1,
+			Blocking => 0,
+		);
+	} else {
+		require IO::Socket::UNIX;
+		$socket = IO::Socket::UNIX->new(
+			Type => IO::Socket::UNIX::SOCK_STREAM(),
+			Local => $port,
+			Listen => 1,
+			Blocking => 0,
+		);
+	}
+
+	my $self = \$socket;
+	bless $self, $class;
+
+	RSGet::IO_Event->add_read( $socket, $self, "_client" );
+
+	return $self;
+}
+
+
+# INTERNAL: accept new connection and create client
+sub _client
+{
+	my $self = shift;
+	my $time = shift;
+
+	my $h = $$self;
+	my $cli = $h->accept();
+	return unless $cli;
+
+	require RSGet::SCGI_Client;
+	RSGet::SCGI_Client->create( $cli );
+}
+
+
+=head2 $server->delete();
+
+Delete scgi server.
+
+=cut
+sub delete
+{
+	my $self = shift;
+	RSGet::IO_Event->remove( $$self );
+}
+
+sub DESTROY
+{
+	my $self = shift;
+	$self->delete();
+}
+
+1;
+
+# vim: ts=4:sw=4:fdm=marker
