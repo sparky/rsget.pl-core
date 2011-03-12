cee7151cf6cc8a1aa0922a39a9575cf8f6c0a0cb
sparky <sparky@pld-linux.org>
2011-03-12 16:08:13 +0100

    - use RSGet::Mux for event loop

--- test/http_server.pl
+++ test/http_server.pl
@@ -4,12 +4,13 @@ use strict;
 use warnings;
 use RSGet::Cnt;
 use RSGet::HTTP_Server;
+use RSGet::Mux;
 
 $SIG{CHLD} = sub {
 	my $kid;
 	do {
 		$kid = waitpid -1, RSGet::Cnt::WNOHANG;
-		print "\rChild $kid exited: $?\n" if $kid > 0;
+		#print "\rChild $kid exited: $?\n" if $kid > 0;
 	} while ( $kid > 0 );
 };
 
@@ -17,14 +18,12 @@ my $server = RSGet::HTTP_Server->create( 8080 );
 
 my @c = qw(\ | / -);
 my $i = 0;
-while ( 1 ) {
-	eval {
-		RSGet::IO_Event::_perform();
+
+RSGet::Mux::add_short
+	_fly => sub {
+		print "\r" . $c[ $i = ( $i + 1 ) % 4 ];
+		STDOUT->flush();
 	};
-	warn "_perform() died: $@" if $@;
-	select undef, undef, undef, 0.05;
-	print "\r" . $c[ $i = ( $i + 1) % 4 ];
-	STDOUT->flush();
-}
+RSGet::Mux::main_loop();
 
 # vim: ts=4:sw=4
