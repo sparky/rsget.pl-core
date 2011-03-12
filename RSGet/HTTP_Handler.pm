c232a39526d8280f990347e0891efd859510075b
sparky <sparky@pld-linux.org>
2011-03-12 17:42:04 +0100

    - cleanups

--- RSGet/HTTP_Handler.pm
+++ RSGet/HTTP_Handler.pm
@@ -57,8 +57,7 @@ sub _send_file
 	$file =~ s#^/file/##;
 
 	unless ( -r $file and -f $file ) {
-		$req->{code} = 404;
-		return "File '$file' not found\n";
+		throw '404: File '%s' not found', $file;
 	}
 
 
@@ -67,15 +66,13 @@ sub _send_file
 	my $end = $size - 1;
 
 	my $h = $req->{h_in};
-	if ( exists $h->{range} ) {
-		if ( $h->{range} =~ /bytes=(\d+)-(\d+)?/ ) {
-			$skip = $1;
-			$end = $2 || $size - 1;
-			my $cr = sprintf "bytes %d-%d/%d", $skip, $end, $size;
-			$size = $end - $skip + 1;
-			$req->{h_out}->{content_range} = $cr;
-			$req->{code} = 206; # partial content
-		}
+	if ( exists $h->{range} and $h->{range} =~ /bytes=(\d+)-(\d+)?/ ) {
+		$skip = 0+$1;
+		$end = $2 || $size - 1;
+		my $cr = sprintf "bytes %d-%d/%d", $skip, $end, $size;
+		$size = $end - $skip + 1;
+		$req->{h_out}->{content_range} = $cr;
+		$req->{code} = 206; # partial content
 	}
 
 	$req->{h_out}->{content_length} = $size;
@@ -102,52 +99,67 @@ sub _readfile
 	throw '416: cannot end before start position'
 		if $end <= $skip;
 
+	my $toread = $end - $skip;
+
+	# if file is small we read it directly and return the data
+	if ( $toread <= BLOCK_SIZE ) {
+		open my $fin, "<", $path;
+		sysseek $fin, $skip, RSGet::Cnt::SEEK_SET
+			if $skip;
+
+		sysread $fin, my $buf, $toread;
+		return $buf;
+	}
+
+	# for large files we create a file-reading sub process
+	# and return an iterator
+	# NOTE: sub process is required because reading files always blocks,
+	#       but we don't want it to block
+
 	pipe my $rh, my $wh;
 	my $pid = fork();
 	unless ( defined $pid ) {
 		throw '500: cannot fork';
 	}
 
-	my $toread = $end - $skip;
-	if ( not $pid ) {
-		# child
-		# TODO: maybe ionice ?
+	if ( $pid ) {
+		# parent
+		close $wh;
 
-		$SIG{__DIE__} = sub {
-			exec "false"
-				or 1;
-
-			require POSIX;
-			POSIX::_exit( 0 );
+		my $io = RSGet::IO->new( $rh );
+		return sub {
+			return $io->read( BLOCK_SIZE );
 		};
-
-		close $rh;
-
-		open my $fin, "<", $path;
-
-		sysseek $fin, $skip, RSGet::Cnt::SEEK_SET
-			if $skip;
-
-		my $buf = " " x BLOCK_SIZE;
-		$buf = "";
-		while ( $toread > BLOCK_SIZE ) {
-			sysread $fin, $buf, BLOCK_SIZE;
-			syswrite $wh, $buf;
-			$toread -= BLOCK_SIZE;
-		}
-		sysread $fin, $buf, $toread;
-		syswrite $wh, $buf;
-
-		# we must exit without calling any destructors
-		exec "true"
-			or die;
 	}
-	close $wh;
 
-	my $io = RSGet::IO->new( $rh );
-	return sub {
-		return $io->read( BLOCK_SIZE );
+	# child
+	# TODO: maybe ionice ?
+
+	close $rh;
+	local $SIG{__DIE__} = sub {
+		exec "false"
+			or 1;
+
+		require POSIX;
+		POSIX::_exit( 0 );
 	};
+
+	open my $fin, "<", $path;
+	sysseek $fin, $skip, RSGet::Cnt::SEEK_SET
+		if $skip;
+
+	my $buf;
+	while ( $toread > BLOCK_SIZE ) {
+		sysread $fin, $buf, BLOCK_SIZE;
+		syswrite $wh, $buf;
+		$toread -= BLOCK_SIZE;
+	}
+	sysread $fin, $buf, $toread;
+	syswrite $wh, $buf;
+
+	# must exit without calling any destructors
+	exec "true"
+		or die;
 }
 
 1;
